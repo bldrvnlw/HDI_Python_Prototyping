d9d751dde0681614bdd56fd825da7b7dcbf9aa85
Baldur van Lew <b.van_lew@lumc.nl>
2025-07-29 17:44:06 +0200

    Add code to optionall use probabilities from nptsne
    needs debugging - gives bad results

--- src/hdi_proto.py
+++ src/hdi_proto.py
@@ -52,6 +52,7 @@ from utils.data_sources import (
     get_mouse_Zheng,
     get_hypomap,
     get_xmas_tree,
+    get_wikiword_350000,
 )
 
 from utils.base import pairwise_l2_distances
@@ -80,53 +81,93 @@ num_iterations = 1000  # was 1000
 # num_points = 1306127  # all in Zheng 1306127
 # X, y, colors, unique_colors = get_mouse_Zheng(num_points=num_points)
 perplexity = 30  # was 30
-num_points = 20000  # was 70000 all in MNIST
+num_points = 70000  # was 70000 all in MNIST
 X, y, colors, unique_colors = get_MNIST(num_points=num_points)
+# Wikiword
+# perplexity = 90  # was 30
+# num_points = 100000  # was 70000 all in MNIST
+# X, y, colors, unique_colors = get_wikiword_350000(num_points=num_points)
 
-
+metrics = False
 knn_algorithm = "HNSW"  # Annoy or HNSW
 # randomly initialize the embedding
 points = get_random_uniform_circular_embedding(num_points, 0.1)
 
 perplexity_multiplier = 3  # was 3
 nn = perplexity * perplexity_multiplier
-if knn_algorithm == "Annoy":
-    print("Using Annoy")
-    distances, neighbours, indices = compute_annoy_distances(
-        data=X,
-        num_trees=int(math.sqrt(num_points)),
-        nn=nn,
-    )
-else:
-    print("Using HNSW")
-    distances, neighbours, indices = compute_hnsw_distances(data=X, nn=nn)
 
-# # print(f"dist {distances.shape} distances {distances}")
-# # print(f"indices {indices.shape} indices {indices}")
-# # print(f"neigh {neighbours.shape} neighbours {neighbours}")
+# perform old nptsne
+tsne = TextureTsne(True, perplexity=perplexity, knn_algorithm=KnnAlgorithm.HNSW)
+embed_nptsne = tsne.fit_transform(X)
+embed_nptsne = embed_nptsne.reshape(num_points, 2)
+tembed_nptsne = NNPointsTorch(embed_nptsne)
+nptsne_klvalues = tsne.kl_values
 
-# # Compute the perplexity probabilities
-P, sigmas = compute_perplexity_probs_numba(distances, perplexity=perplexity)
-# Symmetrize and flatten to indexes 1D arrays
-neighbours, probabilities, indices = symmetrize_P(P, neighbours, nn)
-print(
-    "Ratio of symmetrized High Dimensional Probability to num_points\n"
-    f"(should be approx 1): {probabilities.sum()/num_points}"
-)
-# # # print(f"Perplexity matrix {P.shape} sigmas {sigmas.shape}")
+nptsne_probabilities = tsne.transition_matrix
 
+num_rows = len(nptsne_probabilities)
 
-# Or Using OpenTSNE
-# neighbours, probabilities, indices = getProbabilitiesOpenTSNE(X, perplexity=perplexity)
-# Create a manager for the shaders
-# and persistent buffers
+neigh_rows = []
+prob_rows = []
+nptsne_indices = np.zeros(num_rows * 2, dtype=int)
+offset = 0
+for pos, point_prob_tups in enumerate(nptsne_probabilities):
+    nptsne_neigh, nptsne_prob = zip(*point_prob_tups)
+    neigh_rows.append(nptsne_neigh)
+    prob_rows.append(nptsne_prob)
+    nptsne_indices[pos * 2] = offset
+    nptsne_indices[pos * 2 + 1] = len(nptsne_neigh)
+    offset += len(nptsne_neigh)
 
+nptsne_prob_np = np.array(prob_rows)
+nptsne_prob_np = nptsne_prob_np.flatten()
+nptsne_neigh_np = np.array(neigh_rows)
+nptsne_neigh_np = nptsne_neigh_np.flatten()
 
 prob_matrix = LinearProbabilityMatrix(
-    neighbours=neighbours,
-    probabilities=probabilities,
-    indices=indices,
+    neighbours=nptsne_neigh_np,
+    probabilities=nptsne_prob_np,
+    indices=nptsne_indices,
 )
+
+self_own_probs = True
+
+if self_own_probs:
+    if knn_algorithm == "Annoy":
+        print("Using Annoy")
+        distances, neighbours, indices = compute_annoy_distances(
+            data=X,
+            num_trees=int(math.sqrt(num_points)),
+            nn=nn,
+        )
+    else:
+        print("Using HNSW")
+        distances, neighbours, indices = compute_hnsw_distances(data=X, nn=nn)
+
+    # # print(f"dist {distances.shape} distances {distances}")
+    # # print(f"indices {indices.shape} indices {indices}")
+    # # print(f"neigh {neighbours.shape} neighbours {neighbours}")
+
+    # # Compute the perplexity probabilities
+    P, sigmas = compute_perplexity_probs_numba(distances, perplexity=perplexity)
+    # Symmetrize and flatten to indexes 1D arrays
+    neighbours, probabilities, indices = symmetrize_P(P, neighbours, nn)
+    print(
+        "Ratio of symmetrized High Dimensional Probability to num_points\n"
+        f"(should be approx 1): {probabilities.sum()/num_points}"
+    )
+    # # # print(f"Perplexity matrix {P.shape} sigmas {sigmas.shape}")
+
+    # Or Using OpenTSNE
+    # neighbours, probabilities, indices = getProbabilitiesOpenTSNE(X, perplexity=perplexity)
+    # Create a manager for the shaders
+    # and persistent buffers
+
+    prob_matrix = LinearProbabilityMatrix(
+        neighbours=neighbours,
+        probabilities=probabilities,
+        indices=indices,
+    )
 
 
 mgr = Manager()
@@ -141,7 +182,7 @@ persistent_tensors = PersistentTensors(
 start_exaggeration = 4.0  # was 4.0  # should decay at a certain point
 end_exaggeration = 1.0
 decay_start = 250  # was 250
-decay_length = 200  # was 200
+decay_length = 150  # was 201500
 
 klvalues = np.zeros((num_iterations,))
 
@@ -149,6 +190,7 @@ klvalues = np.zeros((num_iterations,))
 # plt.scatter(points[:, 0], points[:, 1], c=colors, alpha=0.7)
 a_num_points = np.array([num_points], dtype=np.uint32)
 persistent_tensors.set_tensor_data(ShaderBuffers.NUM_POINTS, a_num_points)
+
 
 print("Starting GPU iterations")
 import time
@@ -176,8 +218,8 @@ for i in range(num_iterations):
 
     MINIMUM_FIELDS_SIZE = 5
     RESOLUTION_SCALING = 2
-    range_x = bounds[1][0] - bounds[0][0]
-    range_y = bounds[1][1] - bounds[0][1]
+    range_x = abs(bounds[1][0] - bounds[0][0])
+    range_y = abs(bounds[1][1] - bounds[0][1])
 
     # assume adaptive resolution (scales with points range) with a minimum size
     width = int(max(RESOLUTION_SCALING * range_x, MINIMUM_FIELDS_SIZE))
@@ -295,68 +337,74 @@ tX = NNPointsTorch(X)
 reducer = umap.UMAP(n_neighbors=perplexity * perplexity_multiplier)
 UMAPembedding = reducer.fit_transform(X)
 tUMAPembedding = NNPointsTorch(UMAPembedding)
-# perform old nptsne
-tsne = TextureTsne(True, perplexity=30, knn_algorithm=KnnAlgorithm.HNSW)
-embed_nptsne = tsne.fit_transform(X)
-embed_nptsne = embed_nptsne.reshape(num_points, 2)
-tembed_nptsne = NNPointsTorch(embed_nptsne)
-nptsne_klvalues = tsne.kl_values
-
-NNP_VK = neighborhood_preservation_torch(tX, tXY, nr_neighbors=90)
-NNP_UMAP = neighborhood_preservation_torch(embed=tUMAPembedding, X=tX, nr_neighbors=90)
-NNP_NPTSNE = neighborhood_preservation_torch(embed=tembed_nptsne, X=tX, nr_neighbors=90)
 
 
-int_labels = y.astype(np.int32)
-NHIT_VK = neighborhood_hit_torch(xy, int_labels, nr_neighbors=90)
-NHIT_UMAP = neighborhood_hit_torch(UMAPembedding, int_labels, nr_neighbors=90)
-NHIT_NPTSNE = neighborhood_hit_torch(embed_nptsne, int_labels, nr_neighbors=90)
+if metrics:
+    NNP_VK = neighborhood_preservation_torch(tX, tXY, nr_neighbors=90)
+    NNP_UMAP = neighborhood_preservation_torch(
+        embed=tUMAPembedding, X=tX, nr_neighbors=90
+    )
+    NNP_NPTSNE = neighborhood_preservation_torch(
+        embed=tembed_nptsne, X=tX, nr_neighbors=90
+    )
 
-tDX = NNPointsTorch(tX.get_pairwise_l2_distances().cpu().numpy())
-tDXY = NNPointsTorch(tXY.get_pairwise_l2_distances().cpu().numpy())
-tDUMAPembedding = NNPointsTorch(
-    tUMAPembedding.get_pairwise_l2_distances().cpu().numpy()
-)
-tDembed_nptsne = NNPointsTorch(tembed_nptsne.get_pairwise_l2_distances().cpu().numpy())
+    if y is not None:
+        int_labels = y.astype(np.int32)
+        NHIT_VK = neighborhood_hit_torch(xy, int_labels, nr_neighbors=90)
+        NHIT_UMAP = neighborhood_hit_torch(UMAPembedding, int_labels, nr_neighbors=90)
+        NHIT_NPTSNE = neighborhood_hit_torch(embed_nptsne, int_labels, nr_neighbors=90)
+    else:
+        NHIT_VK = ""
+        NHIT_UMAP = ""
+        NHIT_NPTSNE = ""
 
-SPEARMANR_VK, STRESS_VK = get_spearman_and_stress(tDX, tDXY)
-SPEARMANR_UMAP, STRESS_UMAP = get_spearman_and_stress(tDX, tDUMAPembedding)
-SPEARMANR_NPTSNE, STRESS_NPTSNE = get_spearman_and_stress(tDX, tDembed_nptsne)
+    tDX = NNPointsTorch(tX.get_pairwise_l2_distances().cpu().numpy())
+    tDXY = NNPointsTorch(tXY.get_pairwise_l2_distances().cpu().numpy())
 
-TRUST_VK = trustworthiness_torch(tX, tXY)
-TRUST_UMAP = trustworthiness_torch(tX, tUMAPembedding)
-TRUST_NPTSNE = trustworthiness_torch(tX, tembed_nptsne)
+    tDUMAPembedding = NNPointsTorch(
+        tUMAPembedding.get_pairwise_l2_distances().cpu().numpy()
+    )
+    tDembed_nptsne = NNPointsTorch(
+        tembed_nptsne.get_pairwise_l2_distances().cpu().numpy()
+    )
+    SPEARMANR_VK, STRESS_VK = get_spearman_and_stress(tDX, tDXY)
+    SPEARMANR_UMAP, STRESS_UMAP = get_spearman_and_stress(tDX, tDUMAPembedding)
+    SPEARMANR_NPTSNE, STRESS_NPTSNE = get_spearman_and_stress(tDX, tDembed_nptsne)
 
-CONT_VK = continuity_torch(tX, tXY)
-CONT_UMAP = continuity_torch(tX, tUMAPembedding)
-CONT_NPTSNE = continuity_torch(tX, tembed_nptsne)
+    TRUST_VK = trustworthiness_torch(tX, tXY)
+    TRUST_UMAP = trustworthiness_torch(tX, tUMAPembedding)
+    TRUST_NPTSNE = trustworthiness_torch(tX, tembed_nptsne)
 
-print("**************")
-print(f"NNP value VK: {NNP_VK}")  # approx same as area under RNX curve
-print(f"NHIT value VK: {NHIT_VK}")
-print(f"SPEARMAN-R value VK: {SPEARMANR_VK}")
-print(f"STRESS value VK: {STRESS_VK}")
-print(f"TRUSTWORTHINESS value VK: {TRUST_VK}")
-print(f"CONTINUITY value VK: {CONT_VK}")
-print("**************")
-print(f"NNP value UMAP: {NNP_UMAP}")
-print(f"NHIT value UMAP: {NHIT_UMAP}")
-print(f"SPEARMAN-R value UMAP: {SPEARMANR_UMAP}")
-print(f"STRESS value UMAP: {STRESS_UMAP}")
-print(f"TRUSTWORTHINESS value VK: {TRUST_UMAP}")
-print(f"CONTINUITY value VK: {CONT_UMAP}")
-print("**************")
-print(f"NNP value nptsne: {NNP_NPTSNE}")
-print(f"NHIT value nptsne: {NHIT_NPTSNE}")
-print(f"SPEARMAN-R value nptsne: {SPEARMANR_NPTSNE}")
-print(f"STRESS value nptsne: {STRESS_NPTSNE}")
-print(f"TRUSTWORTHINESS value VK: {TRUST_NPTSNE}")
-print(f"CONTINUITY value VK: {CONT_NPTSNE}")
-print("**************")
+    CONT_VK = continuity_torch(tX, tXY)
+    CONT_UMAP = continuity_torch(tX, tUMAPembedding)
+    CONT_NPTSNE = continuity_torch(tX, tembed_nptsne)
 
-neighbors = [int(perplexity / 3), perplexity, perplexity * 3, perplexity * 9]
-trust = [trustworthiness_torch(tX, tXY, n_neighbors=int(k)) for k in neighbors]
-print(f"Trustworthiness VK for neighbors {neighbors} : {trust}")
+    print("**************")
+    print(f"NNP value VK: {NNP_VK}")  # approx same as area under RNX curve
+    print(f"NHIT value VK: {NHIT_VK}")
+    print(f"SPEARMAN-R value VK: {SPEARMANR_VK}")
+    print(f"STRESS value VK: {STRESS_VK}")
+    print(f"TRUSTWORTHINESS value VK: {TRUST_VK}")
+    print(f"CONTINUITY value VK: {CONT_VK}")
+    print("**************")
+    print(f"NNP value UMAP: {NNP_UMAP}")
+    print(f"NHIT value UMAP: {NHIT_UMAP}")
+    print(f"SPEARMAN-R value UMAP: {SPEARMANR_UMAP}")
+    print(f"STRESS value UMAP: {STRESS_UMAP}")
+    print(f"TRUSTWORTHINESS value VK: {TRUST_UMAP}")
+    print(f"CONTINUITY value VK: {CONT_UMAP}")
+    print("**************")
+    print(f"NNP value nptsne: {NNP_NPTSNE}")
+    print(f"NHIT value nptsne: {NHIT_NPTSNE}")
+    print(f"SPEARMAN-R value nptsne: {SPEARMANR_NPTSNE}")
+    print(f"STRESS value nptsne: {STRESS_NPTSNE}")
+    print(f"TRUSTWORTHINESS value VK: {TRUST_NPTSNE}")
+    print(f"CONTINUITY value VK: {CONT_NPTSNE}")
+    print("**************")
+
+    neighbors = [int(perplexity / 3), perplexity, perplexity * 3, perplexity * 9]
+    trust = [trustworthiness_torch(tX, tXY, n_neighbors=int(k)) for k in neighbors]
+    print(f"Trustworthiness VK for neighbors {neighbors} : {trust}")
 
 # QNX = compute_coranking_matrix(data_ld=xy, data_hd=X)
 
@@ -366,10 +414,12 @@ axs[0][0].scatter(
     UMAPembedding[:, 0],
     UMAPembedding[:, 1],
     c=colors,
-    s=40 / math.log10(num_points),
-    alpha=0.7,
+    s=10 / math.log10(num_points),
+    alpha=0.5,
 )
-axs[0][0].set_title(f"UMAP nnp: {NNP_UMAP}")
+
+if metrics:
+    axs[0][0].set_title(f"UMAP nnp: {NNP_UMAP}")
 axs[0][0].set_ylabel("embeddings: ", size="large")
 
 
@@ -377,14 +427,15 @@ axs[0][1].scatter(
     embed_nptsne[:, 0],
     embed_nptsne[:, 1],
     c=colors,
-    s=40 / math.log10(num_points),
-    alpha=0.7,
+    s=10 / math.log10(num_points),
+    alpha=0.5,
 )
-axs[0][1].set_title(f"nptsne nnp: {NNP_NPTSNE}")
+if metrics:
+    axs[0][1].set_title(f"nptsne nnp: {NNP_NPTSNE}")
 
 
 axs[0][2].scatter(
-    xy[:, 0], xy[:, 1], c=colors, s=40 / math.log10(num_points), alpha=0.7
+    xy[:, 0], xy[:, 1], c=colors, s=10 / math.log10(num_points), alpha=0.5
 )
 
 custom = [
@@ -406,7 +457,8 @@ axs[0][2].legend(
     prop={"size": 8},
 )
 
-axs[0][2].set_title(f"HDILib VK nnp: {NNP_VK}")
+if metrics:
+    axs[0][2].set_title(f"HDILib VK nnp: {NNP_VK}")
 
 axs[1][0].set_ylabel("KL-div: ", size="large")
 axs[1][1].plot(range(0, num_iterations), nptsne_klvalues)
